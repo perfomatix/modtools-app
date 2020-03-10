5e6efe4776ed132b00e031520beb1629fd520a71
Sijo Kuriakose <sijo.kuriakose@perfomatix.com>
2020-03-10 12:28:46 +0530

    fix: file for api urls

---
+++ src/app/core/api-constants/api-url.ts
@@ -0,0 +1,3 @@
+export class ApiUrl {
+    public static classifyText = `/classify/text`
+}
--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ApiUrl } from '@core/api-constants/api-url';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class SearchService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  
+  searchService(searchString: string) {
+    return this.httpClient.post(ApiUrl.classifyText, searchString);
+  }
+
 }
