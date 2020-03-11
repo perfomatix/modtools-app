f72585da41eccd2d0fae8402c586b0266a6ce6e5
swathi <swathi.krishna@perfomatix.com>
2020-03-11 15:41:02 +0530

    .

--- src/app/generic-view/text-diagnose/text-diagnose.component.ts
+++ src/app/generic-view/text-diagnose/text-diagnose.component.ts
@@ -6,12 +6,19 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./text-diagnose.component.less']
 })
 export class TextDiagnoseComponent implements OnInit {
-
+show:any;
+indexVal:any;
   @Input() response: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  enter(e) {
+   this.indexVal =  e; 
+}
 
+leave(e) {
+  this.indexVal = '';
+}
 }
